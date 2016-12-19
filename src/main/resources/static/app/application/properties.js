ed0f3f48ca773666a6cecccedcae4e9a602e387f
Júlio L. Jr <julio.leitao.jr@gmail.com>
2016-12-19 20:00:19 -0300

    conf file to deploy

--- src/main/resources/static/app/application/properties.js
+++ src/main/resources/static/app/application/properties.js
@@ -1,4 +1,4 @@
 var Properties = {
     // webserviceAddress: 'http://150.165.98.43:8080'
-    webserviceAddress: 'http://localhost:8080/rest'
+    webserviceAddress: 'https://playpal2.herokuapp.com/rest'
 };
